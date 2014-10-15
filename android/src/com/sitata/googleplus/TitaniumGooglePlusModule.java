a95ed7a0280a26b2ef73868b795bd32b94d4d287
Adam St. John <astjohn@gmail.com>
2014-10-15 18:16:13 -0400

    Preliminary android structure.

--- android/src/com/sitata/googleplus/TitaniumGooglePlusModule.java
+++ android/src/com/sitata/googleplus/TitaniumGooglePlusModule.java
@@ -17,19 +17,40 @@ import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.IntentSender.SendIntentException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.GoogleApiClient.Builder;
+import com.google.android.gms.common.ConnectionResult;
+
+import com.google.android.gms.plus.Plus;
+
+import com.google.android.gms.plus.PlusClient;
+
+
 
 @Kroll.module(name="TitaniumGooglePlus", id="com.sitata.googleplus")
-public class TitaniumGooglePlusModule extends KrollModule
+public class TitaniumGooglePlusModule extends KrollModule implements
+	ConnectionCallbacks, OnConnectionFailedListener
 {
 
 	// Standard Debugging variables
 	private static final String LCAT = "TitaniumGooglePlusModule";
 	private static final boolean DBG = TiConfig.LOGD;
 	
+	private static final int RC_SIGN_IN = 0;
 	private String mClientId;
 	private Object[] mScopes;
 	private KrollEventCallback successCallback;
 	private KrollEventCallback errorCallback;
+	
+	/* Client used to interact with Google APIs. */
+	private static GoogleApiClient mGoogleApiClient;
+	
+	private Boolean mIntentInProgress;
 
 	// You can define constants with @Kroll.constant, for example:
 	// @Kroll.constant public static final String EXTERNAL_NAME = value;
@@ -37,6 +58,13 @@ public class TitaniumGooglePlusModule extends KrollModule
 	public TitaniumGooglePlusModule()
 	{
 		super();
+		
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addConnectionCallbacks(this)
+        .addOnConnectionFailedListener(this)
+        .addApi(Plus.API)
+        .addScope(Plus.SCOPE_PLUS_LOGIN)
+        .build();
 	}
 
 	@Kroll.onAppCreate
@@ -44,8 +72,60 @@ public class TitaniumGooglePlusModule extends KrollModule
 	{
 		Log.d(LCAT, "inside onAppCreate");
 		// put module init code that needs to run when the application is created
-	}
+		
 
+	}
+	
+	@Override
+	public void onStart(Activity activity) {
+		super.onStart(activity);
+		mGoogleApiClient.connect();
+	}
+	
+	@Override
+	public void onStop(Activity activity) {
+		super.onStop(activity);
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+	}
+	
+	
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// Called when there was an error connecting the client to the service.
+	  if (!mIntentInProgress && result.hasResolution()) {
+	    try {
+	      mIntentInProgress = true;
+	      startIntentSenderForResult(result.getResolution().getIntentSender(),
+	          RC_SIGN_IN, null, 0, 0, 0);
+	    } catch (SendIntentException e) {
+	      // The intent was canceled before it was sent.  Return to the default
+	      // state and attempt to connect to get an updated ConnectionResult.
+	      mIntentInProgress = false;
+	      mGoogleApiClient.connect();
+	    }
+	  }
+	}
+	
+	@Override
+	public void onConnected(Bundle connectionHint) {
+	  // We've resolved any connection errors.  mGoogleApiClient can be used to
+	  // access Google APIs on behalf of the user.
+		
+		// After calling connect(), this method will be invoked asynchronously 
+		// when the connect request has successfully completed.
+	}
+	
+	@Override
+	public void onConnectionSuspended(int cause) {
+		
+	}
+	
+	
+	
+	
+	
 	// Methods
 	@Kroll.method
 	public void signin(KrollDict props)
